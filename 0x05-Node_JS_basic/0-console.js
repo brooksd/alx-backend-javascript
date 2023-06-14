4be05d06e86b9e93e047931d1c2325f970fb1b40
Donald Brooks <brooksd079@gmail.com>
2023-06-14 13:25:01 +0300

    console log

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Donald Brooks <https://github.com/brooksd>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
